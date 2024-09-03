0234536b8439297a1dd9b3abff948cec45700e81
LegumeEmittingDiode <legume_emitting_diode@protonmail.com>
2024-09-02 23:58:25 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,6 +6,8 @@
 `default_nettype none
 
 module tt_um_tiny_pll (
+    input wire VDPWR,
+    input wire VGND,
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
